ee4a8f1cd8706b595647f40932d1e85367ddaef8
naruto <1456017852@qq.com>
2019-05-30 00:06:40 +0800

    springboot-web init

---
+++ springboot-day03-web/src/main/java/com/naruto/springboot/SpringbootDay03WebApplication.java
@@ -0,0 +1,13 @@
+package com.naruto.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDay03WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDay03WebApplication.class, args);
+    }
+
+}
