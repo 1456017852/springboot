fb1c9849ce1c2f7a0616ef5751a3bf808bdab8e2
naruto <1456017852@qq.com>
2019-06-20 14:38:09 +0800

    springboot 集成redis

--- springboot-day10-cache/src/main/java/com/naruto/springboot/config/RedisConfig.java
+++ springboot-day10-cache/src/main/java/com/naruto/springboot/config/RedisConfig.java
@@ -1,10 +1,7 @@
 package com.naruto.springboot.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
